ac62ded647aab41ea0ca0f57e445cbf3b9aa8f0f
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2025-02-24 23:15:38 -0500

    added stats

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,26 @@ const fs = require("fs");
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const multer = require('multer');
+const raw = require('body-parser/lib/types/raw');
 const app = express();
 
 const directoryPath = './files/';
 let files = [];
+let downloads = 0;
+let uplaods = 0;
 
 try {
   files = fs.readdirSync(directoryPath);
 } catch (error) {
   console.error('Error reading directory:', error);
+}
+
+try {
+  const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  downloads = data.downloads;
+  uploads = data.uploads;
+} catch (error) {
+  console.error('Error reading data.json:', error);
 }
 
 require('events').EventEmitter.defaultMaxListeners = 15;
@@ -33,8 +44,6 @@ app.use(cors({
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
       cb(null, './files/');
@@ -47,20 +56,64 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+/** 
+ * @api {post} /api/v1/uploadFile Upload a file
+ * @apiName UploadFile
+ * @apiGroup File
+ * @apiVersion 1.0.0
+ * @apiDescription Upload a file to the server.
+ */
 app.post('/api/v1/upload', upload.single('file'), (req, res) => {
-    console.log(req.body);
+    res.send('success');
+    uplaods++;
 }
 );
 
-app.get('/api/v1/fetch', (req, res) => {
+/**
+ * @api {get} /api/v1/fetchFile Fetch a file
+ * @apiName FetchFile
+ * @apiGroup File
+ * @apiVersion 1.0.0
+ * @apiDescription Fetch a random file from the server.
+ */
+app.get('/api/v1/fetchFile', (req, res) => {
     let randomIndex = Math.floor(Math.random() * files.length);
     let randomFile = files[randomIndex];
     res.download(`./files/${randomFile}`);
+    downloads++;
 }
 );
 
+/**
+ * @api {get} /api/v1/fetchFiles fetches the number of downloads
+ * @apiName FetchFiles
+ * @apiGroup File
+ * @apiVersion 1.0.0
+ * @apiDescription Fetches the number of downloads during this session.
+ */
+app.get('/api/v1/fetchDownloads', (req, res) => {
+  res.send(downloads.toString());
+}
+);
 
+/**
+ * @api {get} /api/v1/fetchUploads fetches the number of uploads
+ * @apiName FetchUploads
+ * @apiGroup File
+ * @apiVersion 1.0.0
+ * @apiDescription Fetches the number of uploads during this session.
+ */
+app.get('/api/v1/fetchUploads', (req, res) => {
+  res.send(uplaods.toString());
+}
+);
 
 http.createServer(app).listen(port, () => {
     console.log(`HTTP server up and running on port ${port}`);
 });
+
+// Save data every 10 minutes
+await setInterval(() => {
+  fs.writeFileSync('data.json', JSON.stringify({downloads, uploads}));
+}, 600000);
+
